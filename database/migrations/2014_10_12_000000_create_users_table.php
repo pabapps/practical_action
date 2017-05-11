ba19c0086c2078e225f18817f92fe8a039c2dc68
Raihan Zaman <raihan.zaman@amadeyr.org>
2017-05-11 10:13:43 +0600

    stated workign on the department creation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('phone_num');
             $table->string('gender',20);
+            $table->date('joining_date');
+            $table->date('quitting_date');
             $table->tinyInteger('valid')->default(1);
             $table->integer('line_manager_id')->unsigned()->nullable();
             $table->integer('matrix_manager_id')->unsigned()->nullable();
