842c39b4b7dbe379f9f6d96b9c515a68bc5be232
Raihan Zaman <raihan.zaman@amadeyr.org>
2017-08-22 16:11:38 +0600

    started working on the contact_theme

---
+++ app/ContactsTheme.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContactsTheme extends Model
+{
+    protected $table = 'themes';
+    public $timestamps = true;
+    protected $primaryKey = 'id';
+}
