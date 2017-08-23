b1ce4e9f908662dd533a388c35613430001f4213
Raihan Zaman <raihan.zaman@amadeyr.org>
2017-08-23 12:10:49 +0600

    created the contacts database

---
+++ database/migrations/2017_08_23_052451_create_contact_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('designation');
+            $table->string('organization');
+            $table->integer('category_id');
+            $table->integer('theme_id');
+            $table->string('email1');
+            $table->string('email2');
+            $table->string('mobile');
+            $table->string('phone');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
