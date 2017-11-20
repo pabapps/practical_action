780ffd6c65073de3bb95ed7204cb8f3c1743ab40
Mohammed Raihanuzzaman <raihan.zaman@practicalaction.org.bd>
2017-11-20 16:47:20 +0600

    written the save ligic for the user contract

--- app/UserControllerHelperClass/UserContractHelper.php
+++ app/UserControllerHelperClass/UserContractHelper.php
@@ -12,7 +12,31 @@ class UserContractHelper{
 
 
 	public static function userContract(Request $request,$id){
-		dd($id);
+		
+		$old_contract_data = UserContract::where('user_id',$id)->get();
+
+		$last_data_object = collect($old_contract_data)->last();
+
+		if(is_null($last_data_object)){
+			//since the object is null, means there is no data related to this particular
+			//user contract. Therefore, has to create a new record to make the contract fro this user
+
+			$new_contract = new UserContract;
+
+			$new_contract->user_id = $id;
+
+			$new_contract->start_date = \Carbon\Carbon::createFromFormat('d-m-Y', $request->contract_start_date)->toDateString();
+
+			$new_contract->end_date = \Carbon\Carbon::createFromFormat('d-m-Y', $request->contract_end_date)->toDateString();
+
+			$new_contract->save();
+
+			dd("working on it");
+
+
+		}
+
+
 	}
 
 }
